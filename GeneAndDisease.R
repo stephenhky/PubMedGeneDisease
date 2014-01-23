a797d29b43f17017032b0a0417e74732dfa3bfd4
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2014-01-23 11:58:12 -0500

    Create GeneAndDisease.R

---
+++ GeneAndDisease.R
@@ -0,0 +1,15 @@
+require(RCurl)
+require(XML)
+
+pubmed.engine.prefix = 'http://www.ncbi.nlm.nih.gov/pubmed/?term='
+
+retrieve.pubmed.searchpage<-function(gene, disease) {
+  link.string<-paste(pubmed.engine.prefix, gene, '+AND+', gsub(" ", "+", disease), sep='')
+  #webpage<-getURL(link.string)
+  #readLines(tc<-textConnection(webpage))
+  htmlParse(link.string)
+}
+
+mine.pubmed.num<-function(parsedHTML) {
+  resultstr<-xpathSApply(parsedHTML, '//div//h2[@class = "result_count"]', xmlValue)
+}
