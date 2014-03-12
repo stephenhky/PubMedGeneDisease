dc823edc86ad90be88c59d6730ab1876b3dd9dc6
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2014-03-12 10:45:50 -0400

    Update GeneAndDisease.R

--- GeneAndDisease.R
+++ GeneAndDisease.R
@@ -1,16 +1,47 @@
-require(RCurl)
-require(XML)
+
+
+library(RCurl)
+library(XML)
+
+#a function is written to return a string when put in a gene name and a disease name
 
 pubmed.engine.prefix = 'http://www.ncbi.nlm.nih.gov/pubmed/?term='
 
 retrieve.pubmed.searchpage<-function(gene, disease) {
   link.string<-paste(pubmed.engine.prefix, gene, '+AND+', gsub(" ", "+", disease), sep='')
-  #webpage<-getURL(link.string)
-  #readLines(tc<-textConnection(webpage))
   htmlParse(link.string)
 }
 
-mine.pubmed.num<-function(parsedHTML) {
-  resultstr<-xpathSApply(parsedHTML, '//div//h2[@class = "result_count"]', xmlValue)
+mine.pubmed.content<-function(parsedHTML) {
+  category<-xpathSApply(parsedHTML, '//title', xmlValue)
+  if (grepl("No items found",category)==TRUE){
+    resultstr<-"None"
+  } else if (grepl("[.*]",category)==TRUE){
+    resultstr<-as.numeric(1)
+  } else {
+    alternative<-xpathSApply(parsedHTML, '//ul//li//span//i', xmlValue)
+    if (length(alternative)>0){
+      resultstr<-"None"
+    } else {
+      resultstr<-xpathSApply(parsedHTML, '//div//h2[@class = "result_count"]', xmlValue)
+    }
+  }
   resultstr
 }
+
+mine.pubmed.num<-function(gene, disease) {
+  resultstr<-mine.pubmed.content(retrieve.pubmed.searchpage(gene, disease))
+  result<-0
+  if (grepl("of",resultstr)==TRUE){
+    count<-regexpr("of",resultstr)
+    result<-as.numeric(substr(resultstr,count+3,count+10))
+  }else if (grepl("Results:",resultstr)==TRUE){
+    count<-regexpr(":",resultstr)
+    result<-as.numeric(substr(resultstr,count+1,count+10))
+  }
+  if (resultstr=='None' | is.na(resultstr)) {
+    result<-0
+  }
+  result
+}
+
